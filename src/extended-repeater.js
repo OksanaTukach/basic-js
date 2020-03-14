457119587d09545965f237f7e0e1f3b179e39443
Oksana Tukach <okstukach@gmail.com>
2020-03-14 19:18:01 +0300

    feat: add solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,22 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, {
+    repeatTimes,
+    separator = '+',
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+}) {
+    if(addition != undefined || addition === null) {
+        if (additionRepeatTimes && additionSeparator) {
+            for (let i = 1; i < additionRepeatTimes; i++) {
+                str += addition + additionSeparator;
+            }
+        }
+        str += addition;
+    }
+    let result = str;
+    for (let i = 1; i < repeatTimes; i++) {
+        result += separator + str;
+    }
+    return result;
 };
   
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,68 @@
+
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(direct) {
+        this.direct = direct;
+    }
+    encrypt(message, key) {
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        while(key.length<message.length){
+          key+=key;
+        }
+        let messageWord='';
+        let keyWord='';
+        let indexKey=0;
+        let result='';
+        let j=0;
+        for(let i=0;i<message.length;i++){
+            if(!/[A-Z]/.test(message[i])){
+              result+=message[i];
+              continue;
+            }
+            messageWord=message[i].charCodeAt();
+            keyWord=key[j].charCodeAt();
+            indexKey=messageWord+keyWord-65;
+            if(indexKey>90){
+              indexKey-=26;
+            }
+            j++;
+            result+=String.fromCharCode(indexKey);
+        }
+        if (this.direct === false) {
+            return result.split('').reverse().join('');
+        }
+        return result;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(encryptedMessage, key) {
+        encryptedMessage = encryptedMessage.toUpperCase();
+        key = key.toUpperCase();
+        while(key.length<encryptedMessage.length){
+            key+=key;
+          }
+          let encryptedMessageWord='';
+          let keyWord='';
+          let indexKey=0;
+          let result='';
+          let j=0;
+          for(let i=0;i<encryptedMessage.length;i++){
+              if(!/[A-Z]/.test(encryptedMessage[i])){
+                result+=encryptedMessage[i];
+                continue;
+              }
+              encryptedMessageWord=encryptedMessage[i].charCodeAt();
+              keyWord=key[j].charCodeAt();
+              indexKey=encryptedMessageWord-keyWord+65;
+              if(indexKey<65){
+                indexKey+=26;
+              }
+              j++;
+              result+=String.fromCharCode(indexKey);
+          }  
+        if (this.direct === false) {
+            return result.split('').reverse().join('');
+        }
+        return result;
     }
 }
 
